4ff7fe7e74efb6b2d4748496d84827f71b16281f
AlfieRan <alfie.ranstead@outlook.com>
2022-11-06 10:49:44 +0000

    feat: added 'intensivelogging' to enable and disable debug tools

--- src/pages/api/oauth/callback.ts
+++ src/pages/api/oauth/callback.ts
@@ -15,11 +15,15 @@ import {
 } from "../../../server/constants";
 import { ApiError } from "../../../utils/types/errors";
 
+const intenseLogging = true;
+
 export default async function callback(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
     let failed = false;
+    if (intenseLogging)
+        console.log("Callback request received, checking state...");
     // check if an error was returned
     const response: SpotifyCallbackResponse = {
         code: req.query.code,
@@ -29,6 +33,11 @@ export default async function callback(
 
     if (response.error) {
         res.redirect("/login?error=" + response.error);
+        if (intenseLogging)
+            console.log(
+                "Error returned, redirecting to login...\nError: " +
+                    response.error
+            );
         return;
     }
 
@@ -39,6 +48,8 @@ export default async function callback(
         code: response.code,
         redirect_uri: RedirectUri,
     };
+
+    if (intenseLogging) console.log("Sending auth code to Spotify...");
 
     const accessTokenResponse = await fetch(
         "https://accounts.spotify.com/api/token",
@@ -53,6 +64,8 @@ export default async function callback(
     )
         .then((response) => response.json())
         .catch(async (error) => {
+            if (intenseLogging)
+                console.log("Error at fetch accessTokenResponse: " + error);
             console.log(
                 "SPOTIFY CALLBACK ERROR - ",
                 error,
@@ -60,6 +73,10 @@ export default async function callback(
                 error.response
             );
 
+            if (intenseLogging)
+                console.log(
+                    "Assembling error object, pushing it to redis and redirecting to login..."
+                );
             // create an error object and push it to redis
             const errorData: ApiError = {
                 error: "spotify_callback_error",
@@ -78,6 +95,12 @@ export default async function callback(
     if (failed) return;
 
     if (accessTokenResponse.statusCode >= 400) {
+        if (intenseLogging)
+            console.log(
+                "AccessTokenResponse sent a code >= 400, status code: " +
+                    accessTokenResponse.statusCode
+            );
+
         console.log("SPOTIFY CALLBACK ERROR - ", accessTokenResponse);
         const errorData: ApiError = {
             error: "spotify_callback_error",
@@ -95,12 +118,18 @@ export default async function callback(
         accessTokenResponse as SpotifyTokenResponse;
 
     if (accessToken.access_token === "") {
+        if (intenseLogging)
+            console.log(
+                "Access token is empty, redirecting to error screen..."
+            );
         res.redirect(
             `/error?error=No access token returned, please try again.`
         );
+        return;
     }
 
     // Now it's time to get the users profile so we can store it in redis
+    if (intenseLogging) console.log("Getting user profile...");
     const profileResponseRequest =
         (await fetch("https://api.spotify.com/v1/me", {
             method: "GET",
@@ -109,6 +138,12 @@ export default async function callback(
                 ContentType: "application/json",
             },
         }).catch(async (error) => {
+            if (intenseLogging)
+                console.log(
+                    "Error at fetch profile: " +
+                        error +
+                        "\n Creating error object..."
+                );
             console.log("SPOTIFY CALLBACK ERROR - ", error, error.response);
             const errorData: ApiError = {
                 error: "spotify_callback_error",
@@ -123,25 +158,61 @@ export default async function callback(
 
     if (profileResponseRequest === undefined) return;
 
-    const profileResponse = (await profileResponseRequest.json()).catch(
-        async (error: any) => {
-            console.log("SPOTIFY CALLBACK ERROR - ", error, error.response);
-            const errorData: ApiError = {
-                error: "spotify_callback_error",
-                api: "spotify",
-                statusCode: profileResponseRequest.status,
-                apiResponse: JSON.stringify(profileResponseRequest.body),
-            };
-            await redisClient.lpush("errors", JSON.stringify(errorData));
-            res.redirect(`/error?error=${error}`);
-            return undefined;
-        }
-    );
+    if (profileResponseRequest.status >= 400) {
+        if (intenseLogging)
+            console.log(
+                "Profile request failed...\nAssembling error object..."
+            );
+        console.log("SPOTIFY CALLBACK ERROR - ", profileResponseRequest);
+        const errorData: ApiError = {
+            error: "spotify_callback_error",
+            api: "spotify",
+            statusCode: profileResponseRequest.status,
+            apiResponse: JSON.stringify(profileResponseRequest.body),
+        };
+        await redisClient.lpush("errors", JSON.stringify(errorData));
+        res.redirect(`/error?error=${profileResponseRequest}`);
+        return;
+    }
 
-    if (profileResponse === undefined) return;
+    if (intenseLogging) console.log("Profile request successful, parsing...");
+    let tmp;
+    try {
+        if (intenseLogging) console.log("Parsing...");
+        tmp = await profileResponseRequest.json();
+    } catch (e) {
+        if (intenseLogging) console.log("Error at parsing: " + e);
+        const errorData: ApiError = {
+            error: "spotify_callback_error",
+            api: "spotify",
+            statusCode: profileResponseRequest.status,
+            apiResponse: JSON.stringify(profileResponseRequest.body),
+        };
+        await redisClient.lpush("errors", JSON.stringify(errorData));
+        if (intenseLogging)
+            console.log(
+                "Pushed error obj to redis, Redirecting to error screen..."
+            );
+        res.redirect(`/error?error=${e}`);
+        tmp = undefined;
+    }
+
+    if (tmp === undefined) {
+        if (intenseLogging)
+            console.log(
+                "tmp is undefined, an error has probably occurred, returning..."
+            );
+        return;
+    }
+
+    const profileResponse = tmp;
 
     if (profileResponse.statusCode >= 400) {
-        console.log("SPOTIFY CALLBACK ERROR - ", profileResponse);
+        if (intenseLogging)
+            console.log(
+                "Profile response has an status >= 400 - ",
+                profileResponse
+            );
 
         // create an error object and push it to redis
         const errorData: ApiError = {
@@ -164,6 +235,7 @@ export default async function callback(
     };
 
     // it's time to set the redis profile
+    if (intenseLogging) console.log("Setting redis profile...");
     redisClient.setex(
         fullProfile.id + "_access",
         accessToken.expires_in,
@@ -177,11 +249,14 @@ export default async function callback(
     console.log(`User: ${fullProfile.id} just logged in.`);
 
     // now we can set up the cookie
+    if (intenseLogging) console.log("Setting cookie...");
     const cookie = await createLoginCookie(fullProfile.id);
 
     // set the cookie and redirect user
+    if (intenseLogging) console.log("Setting cookie and redirecting...");
     res.setHeader("Set-Cookie", cookie);
     res.redirect("/dashboard");
+    if (intenseLogging) console.log("Done!");
 }
 
 async function createLoginCookie(id: string) {
