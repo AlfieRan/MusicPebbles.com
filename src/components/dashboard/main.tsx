1eab95bcb54dbd6ae682b57968a687307387da8a
AlfieRan <alfie.ranstead@outlook.com>
2022-11-30 18:10:12 +0000

    patch: fixed mobile loading animation

--- src/components/dashboard/main.tsx
+++ src/components/dashboard/main.tsx
@@ -1,39 +1,55 @@
-import { Center, Text } from "@chakra-ui/react";
+import { Center, Flex, Text } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import Pebbles from "../pebbles/Pebbles";
 
 export function Main() {
     return (
-        <Center h={"full"} w={"full"} flexDir={"column"} overflow={"hidden"}>
-            <motion.div
-                className={
-                    "flex absolute overflow-hidden border-white bg-gray-600 h-96 w-96 justify-center items-center"
-                }
-                animate={{
-                    scale: [5, 1.3, 1.5, 1.5, 0],
-                    borderRadius: ["0%", "0%", "50%", "50%", "50%"],
-                    borderWidth: ["0px", "1px", "2px", "1px", "1px"],
-                    background: [
-                        "#161616",
-                        "#161616",
-                        "#4b5563",
-                        "#4b5563",
-                        "#4b5563",
-                    ],
-                }}
-                transition={{
-                    duration: 4,
-                    delay: 0.5,
-                    ease: "easeInOut",
-                    times: [0, 0.3, 0.4, 1],
-                    borderWidth: {
-                        duration: 0.1,
-                    },
-                }}
-                initial={{ borderRadius: "0%" }}
+        <Center
+            h={"full"}
+            w={"full"}
+            flexDir={"column"}
+            overflow={"hidden"}
+            maxW={"100vw"}
+            maxH={"100vh"}
+        >
+            <Flex
+                pos={"absolute"}
+                overflow={"hidden"}
+                w={"full"}
+                h={"full"}
+                alignItems={"center"}
+                justifyContent={"center"}
             >
-                <Text>Pebbles</Text>
-            </motion.div>
+                <motion.div
+                    className={
+                        "flex absolute overflow-hidden border-white bg-gray-600 h-96 w-96 justify-center items-center"
+                    }
+                    animate={{
+                        scale: [5, 1.3, 1.5, 1.5, 0],
+                        borderRadius: ["0%", "0%", "50%", "50%", "50%"],
+                        borderWidth: ["0px", "1px", "2px", "1px", "1px"],
+                        background: [
+                            "#161616",
+                            "#161616",
+                            "#4b5563",
+                            "#4b5563",
+                            "#4b5563",
+                        ],
+                    }}
+                    transition={{
+                        duration: 4,
+                        delay: 0.5,
+                        ease: "easeInOut",
+                        times: [0, 0.3, 0.4, 1],
+                        borderWidth: {
+                            duration: 0.1,
+                        },
+                    }}
+                    initial={{ borderRadius: "0%" }}
+                >
+                    <Text>Pebbles</Text>
+                </motion.div>
+            </Flex>
             <motion.div
                 className={"flex w-fit h-fit justify-center items-center z-1"}
                 animate={{
